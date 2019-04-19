44fffc958d6580c3a24f999b1ba5df9222900f8b
陈玲 <757080328@qq.com>
2019-04-19 17:59:31 +0800

    新增 list 查询

--- day02/list_type.py
+++ day02/list_type.py
@@ -13,6 +13,39 @@ def list_sel(a):
     # 不填值的话 取到最后一位
     print(a[3:])
 
+def list_del():
+    # 定义一个list
+    alist = ['q',1,'啊啊','8','哼哼','吼吼']
+    # 调用删除方法 不填参数 默认删除最后一位
+    alist.pop()
+    print(alist)
+    # 调用删除方法 填写参数索引值 就可以删除指定参数
+    alist.pop(0)
+    print(alist)
+    # 将切片获取的元素 重新定义一个list保存起来
+    blist = alist[:-3]
+    print(blist)
+
+def list_add():
+    alist = ['a',4,'nihao','8','黑黑','的']
+    # 增加元素，增加在末尾
+    alist.append('哈哈哈')
+    print(alist)
+
+    blist = [1,2,3]
+    # 将一个list 作为元素 添加到list里面
+    alist.append(blist)
+    print(alist)
+
+# 替换
+def list_update():
+    alist = ['a', 4, 'nihao', '8', '黑黑', '的', '哈哈哈', [1, 2, 3]]
+    alist[5] = '白白'
+    print(alist)
+
 if __name__ == '__main__':
     alist=['a',4,'nihao','8','嘻嘻','哈哈']
-    list_sel(alist)
+    # list_sel(alist)
+    # list_del()
+    # list_add()
+    list_update()
